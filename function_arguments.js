3803991c6b4d2405a30f1dc640150aee8dc328bb
Zac Crawford <zwcrawford@gmail.com>
2018-11-30 07:57:27 -0600

    Think this one is done now

--- function_arguments.js
+++ function_arguments.js
@@ -6,24 +6,22 @@ let sentence = ["The","walrus","danced","through","the","trees","in","the","ligh
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement (theWordArray) {
+function addExcitement (theWordArray, mark) {
 
     // Each time the for loop executes, you're going to add one more word to this string
-    let buildMeUp = ""
+    let buildMeUp = "";
 
     for (let i = 0; i < theWordArray.length; i++) {
-        if (i >= 3 && i % 3 === 0){
-        buildMeUp += sentence[i] + "! ";
+        // if a remainder of 3 and greatr than 3, add the desired mark
+        if (i !=0 && i % 3 === 0){
+            buildMeUp += `${sentence[i]}${mark}`;
         } else {
         // Concatenate the new word onto buildMeUp
-        buildMeUp += sentence[i] + " ";
+            buildMeUp += `${sentence[i]} `;
         }
         // Print buildMeUp to the console
-        console.log(buildMeUp)
-        co
+        console.log(buildMeUp);
     }
-
-}
-
+};
 // Invoke the function and pass in the array
-addExcitement(sentence)
+addExcitement(sentence, "? ");
